4126e5fdf99adabf3f079554abc0cf7a80b7dedc
Vladimir <vladimir.medina@ntsprint.com>
2025-05-15 22:18:48 -0400

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,5 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
+        // Verificar si el usuario vmedina@ntsprint.com ya existe
+        if (!\App\Models\User::where('email', 'vmedina@ntsprint.com')->exists()) {
+            \App\Models\User::create([
+                'name' => 'Victor Medina',
+                'email' => 'vmedina@ntsprint.com',
+                'password' => bcrypt('admin'),
+            ]);
+        }
     }
 }
